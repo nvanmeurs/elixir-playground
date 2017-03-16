80a0a1fcc0d64b08f5e2596dd9c4b28aaa9c133c
Nicky van Meurs <nicky.van.meurs@just-bi.nl>
2017-03-16 20:13:32 +0100

    Update FizzBuzz

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,7 +1,8 @@
 defmodule FizzBuzz do
-  defp result(0, _, _), do: "Fizz."
-  defp result(_, 0, _), do: "Buzz."
-  defp result(_, _, x), do: x
+  defp result(0, 0, _), do: "FizzBuzz"
+  defp result(0, _, _), do: "Fizz"
+  defp result(_, 0, _), do: "Buzz"
+  defp result(_, _, n), do: n
 
   def for(n), do: result(rem(n, 3), rem(n, 5), n)
 end
@@ -11,10 +12,10 @@ end
 
 # Result
 # In      Out         Reason
-# 10  =>  Buzz.       [10 is divisible by 5]
+# 10  =>  Buzz        [10 is divisible by 5]
 # 11  =>  11          [11 is not divisible by 3 or 5]
-# 12  =>  Fizz.       [12 is divisible by 3]
+# 12  =>  Fizz        [12 is divisible by 3]
 # 13  =>  13          [13 is not divisible by 3 or 5]
 # 14  =>  14          [14 is not divisible by 3 or 5]
-# 15  =>  Fizz.       [15 is divisible by 3]
+# 15  =>  FizzBuzz    [15 is divisible by 3 and 5]
 # 16  =>  16          [16 is not divisible by 3 or 5]
